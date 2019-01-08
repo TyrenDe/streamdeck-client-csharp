1b66438ec9a7690ada298e04a019c0b1bd6a8596
Shane DeSeranno <tyren@syntheticstream.com>
2019-01-08 15:30:46 -0800

    Clean up property inspector

--- TestPlugin/PropertyInspector/Index.js
+++ TestPlugin/PropertyInspector/Index.js
@@ -1,66 +1,44 @@
-﻿// this is our global websocket, used to communicate from/to Stream Deck software
-// and some info about our plugin, as sent by Stream Deck software
-var websocket = null,
+﻿var websocket = null,
     uuid = null,
     actionInfo = {},
     inInfo = {},
     runningApps = [],
     isQT = navigator.appVersion.includes('QtWebEngine'),
-    onchangeevt = 'onchange'; // 'oninput'; // change this, if you want interactive elements act on any change, or while they're modified
+    onchangeevt = 'onchange';
 
 function connectSocket(inPort, inUUID, inRegisterEvent, inInfo, inActionInfo) {
     uuid = inUUID;
-    // please note: the incoming arguments are of type STRING, so
-    // in case of the inActionInfo, we must parse it into JSON first
     actionInfo = JSON.parse(inActionInfo); // cache the info
     inInfo = JSON.parse(inInfo);
     websocket = new WebSocket('ws://localhost:' + inPort);
 
-    /** Since the PI doesn't have access to native settings
-     * Stream Deck sends some color settings to PI
-     * We use these to adjust some styles (e.g. highlight-colors for checkboxes)
-     */
-    addDynamicStyles(inInfo.colors, 'connectSocket');
-    initPropertyInspector(5);
+    addDynamicStyles(inInfo.colors);
+    initPropertyInspector();
 
-    // if connection was established, the websocket sends
-    // an 'onopen' event, where we need to register our PI
     websocket.onopen = function () {
         var json = {
             event: inRegisterEvent,
             uuid: inUUID
         };
-        // register property inspector to Stream Deck
+
         websocket.send(JSON.stringify(json));
+
+        // Notify the plugin that we are connected
         sendValueToPlugin('propertyInspectorConnected', 'property_inspector');
-        demoCanvas();
     };
 
     websocket.onmessage = function (evt) {
         // Received message from Stream Deck
         var jsonObj = JSON.parse(evt.data);
         var event = jsonObj['event'];
-        if (getPropFromString(jsonObj, 'payload.runningApps') && event === 'sendToPropertyInspector') {
-            sdpiCreateList(document.querySelector('#runningAppsContainer'), {
-                id: 'runningAppsID',
-                label: 'Running Apps',
-                value: jsonObj.payload.runningApps,
-                type: 'list',
-                selectionType: 'no-select'
-            });
-        }
+
+        // TODO: Process and handle events
+        console.log(evt.data);
     };
 }
 
-function initPropertyInspector(initDelay) {
+function initPropertyInspector() {
     prepareDOMElements(document);
-    demoCanvas();
-    /** expermimental carousel is not part of the DOM
-     * so let the DOM get constructed first and then
-     * inject the carousel */
-    setTimeout(function () {
-        initCarousel();
-    }, initDelay || 100);
 }
 
 function revealSdpiWrapper() {
@@ -85,15 +63,17 @@ function sendValueToPlugin(value, param) {
 
 if (!isQT) {
     document.addEventListener('DOMContentLoaded', function () {
-        initPropertyInspector(100);
+        initPropertyInspector();
     });
 }
 
-/** the beforeunload event is fired, right before the PI will remove all nodes */
 window.addEventListener('beforeunload', function (e) {
     e.preventDefault();
+
+    // Notify the plugin we are about to leave
     sendValueToPlugin('propertyInspectorWillDisappear', 'property_inspector');
-    // Don't set a returnValue to the event, otherwise Chromium with throw an error.  // e.returnValue = '';
+
+    // Don't set a returnValue to the event, otherwise Chromium with throw an error.
 });
 
 /** the pagehide event is fired, when the view disappears */
@@ -111,13 +91,6 @@ window.addEventListener('unload', function (event) {
     console.log('%c%s','background: orange; font-size: 22px; font-weight: bold;','window --->> onunload.');
     sendValueToPlugin('propertyInspectorDisconnected', 'property_inspector');
 });
-*/
-
-/** if you prefer, you can apply these listeners to PI's body, like so:
- *
- * <body onpagehide="sendValueToPlugin('propertyInspectorPagehide', 'property_inspector');">
- *
- * <body onunload="sendValueToPlugin('propertyInspectorDisconnected', 'property_inspector');">
 */
 
 /** CREATE INTERACTIVE HTML-DOM
@@ -247,19 +220,7 @@ function handleSdpiItemClick(e, idx) {
     sendValueToPlugin(returnValue, 'sdpi_collection');
 }
 
-function updateKeyForDemoCanvas(cnv) {
-    sendValueToPlugin({
-        key: 'your_canvas',
-        value: cnv.toDataURL()
-    }, 'sdpi_collection');
-}
-
-/** Stream Deck software passes system-highlight color information
- * to Property Inspector. Here we 'inject' the CSS styles into the DOM
- * when we receive this information. */
-
-
-function addDynamicStyles(clrs, fromWhere) {
+function addDynamicStyles(clrs) {
     const node = document.getElementById('#sdpi-dynamic-styles') || document.createElement('style');
     if (!clrs.mouseDownColor) clrs.mouseDownColor = fadeColor(clrs.highlightColor, -100);
     const clr = clrs.highlightColor.slice(0, 7);
@@ -321,55 +282,6 @@ function addDynamicStyles(clrs, fromWhere) {
 
 /** UTILITIES */
 
-/** Helper function to construct a list of running apps
- * from a template string.
- * -> information about running apps is received from the plugin
- */
-
-function sdpiCreateList(el, obj, cb) {
-    if (el) {
-        el.style.display = obj.value.length ? 'block' : 'none';
-        Array.from(document.querySelectorAll(`.${el.id}`)).forEach((subel, i) => {
-            subel.style.display = obj.value.length ? 'flex' : 'none';
-        });
-        if (obj.value.length) {
-            el.innerHTML = `<div class="sdpi-item" ${obj.type ? `class="${obj.type}"` : ''} id="${obj.id || window.btoa(new Date().getTime().toString()).substr(0, 8)}">
-            <div class="sdpi-item-label">${obj.label || ''}</div>
-            <ul class="sdpi-item-value ${obj.selectionType ? obj.selectionType : ''}">
-                    ${obj.value.map(e => `<li>${e}</li>`).join('')}
-                </ul>
-            </div>`;
-            setTimeout(function () {
-                prepareDOMElements(el);
-                if (cb) cb();
-            }, 10);
-            return;
-        }
-    }
-    if (cb) cb();
-};
-
-/** get a JSON property from a (dot-separated) string
- * Works on nested JSON, e.g.:
- * jsn = {
- *      propA: 1,
- *      propB: 2,
- *      propC: {
- *          subA: 3,
- *          subB: {
- *             testA: 5,
- *             testB: 'Hello'
- *          }
- *      }
- *  }
- *  getPropFromString(jsn,'propC.subB.testB') will return 'Hello';
- */
-const getPropFromString = (jsn, str, sep = '.') => {
-    const arr = str.split(sep);
-    return arr.reduce((obj, key) =>
-        (obj && obj.hasOwnProperty(key)) ? obj[key] : undefined, jsn);
-};
-
 /*
     Quick utility to lighten or darken a color (doesn't take color-drifting, etc. into account)
     Usage:
@@ -384,161 +296,3 @@ function fadeColor(col, amt) {
     const b = min(255, max(((num >> 8) & 0x00FF) + amt, 0));
     return '#' + (g | (b << 8) | (r << 16)).toString(16).padStart(6, 0);
 }
-
-/** Quick utility to inject a style to the DOM
- * e.g. injectStyle('.localbody { background-color: green;}')
- */
-function injectStyle(clrs) {
-    const node = document.createElement('style');
-    const tempID = window.btoa(new Date().getTime().toString()).slice(10, 18);
-    node.setAttribute('id', tempID);
-    node.innerHTML = clrs;
-    document.body.appendChild(node);
-    return tempID;
-};
-
-/** Quick utility to return a random color.
- * If the randomly generated string is less than 6 characters
- * pad it with '0'
- */
-function randomColor(prefix) {
-    return (prefix || '') + (((1 << 24) * Math.random()) | 0).toString(16).padStart(6, 0); // just a random color padded to 6 characters
-}
-
-/** CANVAS DEMO */
-
-function demoCanvas() {
-    const touchDevice = (('ontouchstart' in document.documentElement) && (navigator.platform != 'Win32'));
-    const evtDown = touchDevice ? 'touchstart' : 'mousedown';
-    const evtMove = touchDevice ? 'touchmove' : 'mousemove';
-    const evtEnd = touchDevice ? 'touchend' : 'mouseup';
-    const evtX = touchDevice ? 'pageX' : 'clientX';
-    const evtY = touchDevice ? 'pageY' : 'clientY';
-
-    const cnv = document.querySelector('canvas');
-    if (!cnv) return;
-    const ctx = cnv.getContext('2d');
-    if (!ctx) return;
-    function drawRandomCanvas() {
-        const rad = cnv.height / 4;
-        const cX = Math.random() * cnv.width;
-        const cY = Math.random() * cnv.height;
-
-        const grad = ctx.createLinearGradient(0, 0, 0, 170);
-        const clr = randomColor();
-        grad.addColorStop(0, fadeColor(clr, -100));
-        grad.addColorStop(0.5, '#' + clr);
-        grad.addColorStop(1, fadeColor(clr, 100));
-        ctx.fillStyle = grad;
-
-        //  ctx.fillStyle = randomColor();
-        ctx.fillRect(0, 0, cnv.width, cnv.height);
-        ctx.beginPath();
-        ctx.arc(cX, cY, rad, 0, 2 * Math.PI, false);
-        ctx.fillStyle = '#' + randomColor();
-        ctx.fill();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = '#' + randomColor();
-        ctx.stroke();
-    }
-
-    drawRandomCanvas();
-
-    const pos = { x: 0, y: 0 };
-
-    var el = document.querySelector('.sdpi-wrapper');
-    cnv.addEventListener(evtDown, function (e) {
-        if (e.shiftKey) {
-            drawRandomCanvas();
-            return;
-        }
-        pos.x = e[evtX] - cnv.offsetLeft + el.scrollLeft;
-        pos.y = e[evtY] - cnv.offsetTop + el.scrollTop;
-    });
-
-    cnv.addEventListener(evtEnd, function (e) {
-        e.target.value = cnv.toDataURL();
-    });
-
-    cnv.addEventListener(evtMove, function (e) {
-        if (!touchDevice) {
-            if (!e.altKey || e.buttons !== 1) return;
-        }
-        e.preventDefault();
-        ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = '#061261';
-        ctx.moveTo(pos.x, pos.y); // from
-        pos.x = e[evtX] - cnv.offsetLeft + el.scrollLeft;
-        pos.y = e[evtY] - cnv.offsetTop + el.scrollTop;
-        ctx.lineTo(pos.x, pos.y); // to
-        ctx.stroke();
-    });
-
-    updateKeyForDemoCanvas(cnv, cnv);
-}
-
-/** EXPERIMENTAL CAROUSEL  */
-
-function initCarousel() {
-    document.querySelectorAll('.sdpi-item [type=carousel]').forEach((e, i, a) => {
-        var m = e.querySelector('img');
-        e.data = {
-            currentOffset: 0,
-            visibleCards: 3,
-            scrollDistance: m ? m.clientWidth + 10 : 70,
-            numCards: e.querySelectorAll('.card-carousel--card').length,
-            leftNav: e.querySelectorAll('.card-carousel--nav__left'),
-            rightNav: e.querySelectorAll('.card-carousel--nav__right'),
-            atStart: true,
-            atEnd: false
-        };
-
-        e.end = function () {
-            return e.data.currentOffset <= (e.data.scrollDistance * -1) * (e.data.numCards - e.data.visibleCards);
-        };
-
-        const cards = e.querySelector('.card-carousel-cards');
-
-        e.move = function (direction) {
-            if (direction === 1 && !this.data.atEnd) {
-                this.data.currentOffset -= this.data.scrollDistance;
-            } else if (direction === -1 && !this.data.atStart) {
-                this.data.currentOffset += this.data.scrollDistance;
-            }
-
-            if (cards) {
-                cards.setAttribute('style', `transform:translateX(${this.data.currentOffset}px)`);
-                this.data.atStart = this.data.currentOffset === 0;
-                this.data.atEnd = this.end();
-                this.data.leftNav.forEach((ctl) => {
-                    if (!this.data.atStart) ctl.removeAttribute('disabled');
-                    else ctl.setAttribute('disabled', this.data.atStart);
-                });
-                this.data.rightNav.forEach((ctl) => {
-                    if (!this.data.atEnd) ctl.removeAttribute('disabled');
-                    else ctl.setAttribute('disabled', this.data.atEnd);
-                });
-            }
-        };
-
-        e.data.leftNav.forEach((nl) => {
-            nl.onclick = function () {
-                e.move(-1);
-            };
-        });
-
-        e.data.rightNav.forEach((nl) => {
-            nl.onclick = function () {
-                e.move(1);
-            };
-        });
-
-        e.querySelectorAll('.card-carousel--card').forEach((crd, idx) => {
-            crd.onclick = function (evt) {
-                handleSdpiItemClick(crd, idx);
-            };
-        });
-    });
-};
