5b1dc155b84730755b23902dd60bbd955178d8ab
BarRaider <46548278+BarRaider@users.noreply.github.com>
2023-02-12 15:32:52 +0200

    Support SystemDidWakeUp Event

---
+++ streamdeck-client-csharp/Events/SystemDidWakeUpEvent.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace streamdeck_client_csharp.Events
+{
+    /// <summary>
+    /// Payload for SystemDidWakeUp event
+    /// </summary>
+    public class SystemDidWakeUpEvent : BaseEvent
+    {
+    }
+}
